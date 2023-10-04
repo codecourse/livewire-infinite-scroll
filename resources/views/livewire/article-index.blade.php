e77313f9ae9c5b1336a1418a1b29135098b5d46b
Alex Garrett-Smith <alex@codecourse.com>
2023-10-04 09:37:20 +0100

    Episode 08

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -7,7 +7,7 @@
     @endforeach
 
     @if ($this->paginator->hasMorePages())
-        <div x-intersect="$wire.loadMore"></div>
+        <div x-intersect="$wire.loadMore" class="h-12 -translate-y-44"></div>
     @endif
 
     @if ($this->paginator->hasMorePages())
